3306d2bfdab3f9cea0d8e8de878d992769e46e2c
mohammad.m <mohammad7m8@gmail.com>
2024-11-27 23:07:59 +0330

    update tiptap

--- vite.config.js
+++ vite.config.js
@@ -35,6 +35,9 @@ export default defineConfig({
       },
     ],
   },
+  optimizeDeps: {
+    include: [ '@tiptap/pm' ],
+  },
   server: { port: PORT, host: true },
   preview: { port: PORT, host: true },
 });
